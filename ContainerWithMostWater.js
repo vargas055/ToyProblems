4001854581f9e20bba428478949caddff71f3d1b
vargas055 <juanjo220393@gmail.com>
2020-12-11 15:15:49 -0500

    tried to speed up

--- ContainerWithMostWater.js
+++ ContainerWithMostWater.js
@@ -1,26 +1,49 @@
+// /**
+//  * @param {number[]} height
+//  * @return {number}
+//  */
+// var maxArea = function(height) {
+//     let mostWater = 0;
+//     for (let a = 0; a < height.length; a++) {
+//         for (let b = 1; b < height.length; b++) {
+//             if (a !== b) {
+//                 if (height[a] < height[b]) {
+//                     if (height[a] * (b-a) > mostWater) {
+                       
+//                         mostWater = height[a] * (b - a)
+//                     } 
+        
+//                 } else if (height[b] * (b-a) > mostWater) {
+                 
+//                     mostWater = height[b] * (b - a)
+//                 } 
+//             }
+//         }
+//     }
+//     return mostWater
+// }; /// slowwwww!!!!!
+
 /**
  * @param {number[]} height
  * @return {number}
  */
 var maxArea = function(height) {
-    let mostWater = 0;
+    let max = [0, 0]
+    let secondMax = [0, 0]
+
     for (let a = 0; a < height.length; a++) {
-        for (let b = 1; b < height.length; b++) {
-            if (a !== b) {
-                if (height[a] < height[b]) {
-                    if (height[a] * (b-a) > mostWater) {
-                       
-                        mostWater = height[a] * (b - a)
-                    } 
-        
-                } else if (height[b] * (b-a) > mostWater) {
-                 
-                    mostWater = height[b] * (b - a)
-                } 
-            }
+        if (height[a] > max[0]) {
+            secondMax = max
+            max = [height[a], a]
+        } else if (height[a] > secondMax[0]) {
+            secondMax = [height[a], a]
         }
     }
-    return mostWater
+    debugger
+    if (max[1] > secondMax[1]) {
+        return secondMax[0] * (max[1] - secondMax[1]) 
+    }
+    return secondMax[0] * (secondMax[1] - max[1]) 
 };
 
 
