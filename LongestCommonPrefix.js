d03ac609a0a58ad4d09a27562c571e7fe3f8599b
vargas055 <juanjo220393@gmail.com>
2020-12-15 14:26:38 -0500

    longest common prfix

---
+++ LongestCommonPrefix.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    let result = '';
+    let counter = 0;
+    for (let a = 0; a < strs[0].length; a++) {
+        for (let b = 1; b < strs.length; b++) {
+            if (strs[b][a] !== strs[0][a]) {
+                return result
+            }
+        }
+        result += strs[0][a]
+    }
+};
+
+console.log(longestCommonPrefix(["flower","flow","flight"]), 'expect fl')
+console.log(longestCommonPrefix(["dog","racecar","car"]), 'expect ')
+debugger
+console.log()
