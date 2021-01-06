6c5d3ae51fd7d774123c19e95571d3c9868c4390
vargas055 <juanjo220393@gmail.com>
2021-01-06 12:12:41 -0500

    update #2 Python

---
+++ Python/HelloWorld.py
@@ -0,0 +1,41 @@
+
+"Hello World"
+# print('Hello World!!')
+# print('o----')
+# print(' ||||')
+# print('*' * 10)
+##########################
+"Types"
+# price = 10
+# rating = 2.7
+# name = 'Juan'
+# fun = True
+# unfun = False
+# print(price)
+###########################
+# name = 'John Smith'
+# age = 20
+# new_patient = True
+# print(name, age, new_patient)
+###########################
+"Input"
+# name = input('What is your name? ')
+# print('Hi ' + name)
+# name = input('What is your name? ')
+# color = input('What is your favorite color? ')
+# print(name + ' likes ' + color)
+###########################
+"Operations"
+# weight = input('what is your weigth? (Pounds) ')
+# kilo_weight = int(weight)/2.2046
+# print(kilo_weight)
+###########################
+# "If statements"
+# Price = 1000000
+# Good_Credit = False
+
+# if Good_Credit:
+#     print(f'they need to put down {(Price/100)*10}')
+# else:
+#     print(f'they need to put down {(Price/100)*20}')
+############################
