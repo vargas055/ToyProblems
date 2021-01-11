57837dd3ef20d0a5c7ca130dc3df52b49c3b0d12
vargas055 <juanjo220393@gmail.com>
2021-01-11 18:31:24 -0500

    ctor tests full

--- Protactor/conf.js
+++ Protactor/conf.js
@@ -1,5 +1,7 @@
 exports.config = {
     framework: 'jasmine',
     seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['spec.js']
+    specs: ['spec.js'],
+    highlightDelay: 2000,
+    directConnect: false
 }
--- Protactor/spec.js
+++ Protactor/spec.js
@@ -1,12 +1,18 @@
 describe('Protractor Demo App', function() {
     browser.waitForAngularEnabled(false);
-    it('should got to the page and scroll down', function() {
-        browser.get('https://www.digarc.com/')
-        expect(browser.getTitle()).toEqual('Connected Curriculum - DIGARC');
-        browser.executeScript('window.scrollTo(0, document.body.scrollHeight);')
-        .then(()=>{
+    it('should go to the page and scroll down', function() {
+        browser.get('https://www.digarc.com/');
 
-        })
-    });
-  });
+        browser.actions().mouseMove(element(by.id('menu-item-66'))).perform();
+
+        element(by.linkText('Careers')).click();
+
+        element(by.id('search_keywords')).sendKeys("ASSOCIATE");
   
+        element(by.id('search_keywords')).sendKeys(protractor.Key.ENTER);
+
+        browser.sleep(4000);
+
+        expect(browser.getTitle()).toEqual('Careers - DIGARC');
+    });
+});
