38b57c5623e2394b41711c1655d2a913cf304b05
vargas055 <juanjo220393@gmail.com>
2021-01-12 17:45:58 -0500

    Python LIsts

--- Python/HelloWorld.py
+++ Python/HelloWorld.py
@@ -97,11 +97,16 @@
 ############################
 'Lists'
 nums = [1,2,3,5,8,7,6,8,7,5,468,4]
-largest = nums[0]
-for num in nums:
-    if num > largest:
-        largest = num
-print(largest)
+# largest = nums[0]
+# for num in nums:
+#     if num > largest:
+#         largest = num
+# print(largest)
+singles = []
+for number in nums:
+    if number not in singles:
+        singles.insert(0,number)
+print(singles)
 
 
 print('hey')
